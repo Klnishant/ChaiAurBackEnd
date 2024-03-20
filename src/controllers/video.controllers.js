b59a3c206bbcb81839f16bb253a59ed2483ff3bc
Klnishant <nishantkaushal075@gmail.com>
2024-03-20 15:12:22 +0530

    create publishAVideo controller and routes

--- src/controllers/video.controllers.js
+++ src/controllers/video.controllers.js
@@ -4,6 +4,7 @@ import { apiResponse } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { User } from "../models/user.model.js";
 import { video } from "../models/video.model.js"
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 const getAllVideos = asyncHandler( async (req,res)=> {
     const {page=1, limit=10, query, sortBy, sortType, userId} = req.query;
@@ -32,7 +33,7 @@ const getAllVideos = asyncHandler( async (req,res)=> {
         throw new apiError(400,"queryObject not created");
     }
 
-    const result = video.aggregatePaginate({...queryObject, ...filter},options);
+    const result = await video.aggregatePaginate({...queryObject, ...filter},options);
 
     if (!result) {
         throw new apiError(400,"result not find");
@@ -42,6 +43,61 @@ const getAllVideos = asyncHandler( async (req,res)=> {
     .json(new apiResponse(200,result,"video fetched succesfully"))
 });
 
+const publishAVideo = asyncHandler( async (req,res)=> {
+    const {title,description} = req.body;
+
+    if ([title,description].some((field)=>field?.trim()==="")) {
+        throw new apiError(400,"All fields are required");
+    }
+
+    const videoFilePath = req.files?.videoFile[0]?.path;
+    console.log(videoFilePath);
+
+    if (!videoFilePath) {
+        throw new apiError(400,"video is missing");
+    }
+
+    const thumbnailPath = req.files?.thumbnail[0]?.path;
+    console.log(thumbnailPath);
+
+    if (!thumbnailPath) {
+        throw new apiError(400,"thumbnail is missing");
+    }
+
+    const videoFile = await uploadOnCloudinary(videoFilePath);
+    const thumbnail = await uploadOnCloudinary(thumbnailPath);
+
+    console.log(videoFile);
+
+    if (!videoFile) {
+        throw new apiError(500,"video file is required");
+    }
+
+    if(!thumbnail){
+        throw new apiError(500,"thumbnail is required");
+    }
+
+    const videos = await video.create({
+        title,
+        description,
+        videoFile:videoFile.url,
+        thumbnail:thumbnail.url,
+        duration: videoFile.duration,
+    });
+
+    const createdVideo = await video.findById(videos?._id);
+
+    if (!createdVideo) {
+        throw new apiError(500,"videos File not created");
+    }
+
+    return res.status(200)
+    .json(
+        new apiResponse(200,createdVideo,"video file created successfully")
+    );
+});
+
 export {
     getAllVideos,
+    publishAVideo,
 }
--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,9 +1,20 @@
 import { Router } from "express";
-import { getAllVideos } from "../controllers/video.controllers.js";
+import { getAllVideos, publishAVideo } from "../controllers/video.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { upload } from "../middlewares/multer.middlewares.js";
 
 const router = Router();
 
 router.route("/").get(verifyJWT,getAllVideos);
+router.route("/publish").post(upload.fields([
+    {
+        name:"videoFile",
+        maxCount:1,
+    },
+    {
+        name:"thumbnail",
+        maxCount:1,
+    },
+]),verifyJWT,publishAVideo);
 
 export default router;
